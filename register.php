9eacd27fa36acf013ae4d72ea4e107812ea56e79
Serenity Melanson <serenitythefool@gmail.com>
2014-10-17 14:43:18 -0400

    Update register.php

--- register.php
+++ register.php
@@ -3,6 +3,24 @@
 
 
 $firstName = $_POST['firstnamesignup'];
-echo $firstName;
+$lastName = $_POST['lastnamesignup'];
+$userName = $_POST['usernamesignup'];
+$password = $_POST['passwordsignup']; //MD5 this
+$passwordConfirm = $_POST['passwordsignup_confirm'];//MD5 this
+$email = $_POST['emailsignup'];
+$yearOfBirth = $_POST['month'];
+$dayOfBirth = $_POST['day'];
+$MonthOfBirth = $_POST['year'];
+
+
+echo "First Name: " . $firstName . "\n";
+echo "Last Name: " . $lastName . "\n";
+echo "Username: " . $userName . "\n";
+echo "Password: " . $password . "\n";
+echo "Confirmed Password: " . $passwordConfirm . "\n";
+echo "Email: " . $email . "\n";
+echo "Year: " . $yearOfBirth . "\n";
+echo "Day: " . $dayOfBirth . "\n";
+echo "Month: " . $MonthOfBirth "\n";
 
 ?>

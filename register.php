3cae85ce5e51dd3dd05a1a900eb5359acc651575
Serenity Melanson <serenitythefool@gmail.com>
2014-10-17 16:03:13 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,26 +1,30 @@
 <?php
 
+// Create connection
+$con=mysqli_connect("localhost", "Scott", "hello", "skalez_games");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
 
-$firstName = $_POST['firstnamesignup'];
-$lastName = $_POST['lastnamesignup'];
-$userName = $_POST['usernamesignup'];
-$password = $_POST['passwordsignup']; //MD5 this
-$passwordConfirm = $_POST['passwordsignup_confirm'];//MD5 this
-$email = $_POST['emailsignup'];
-$yearOfBirth = $_POST['month'];
-$dayOfBirth = $_POST['day'];
-$MonthOfBirth = $_POST['year'];
+	$firstName = $_POST['firstnamesignup'];
+	$lastName = $_POST['lastnamesignup'];
+	$userName = $_POST['usernamesignup'];
+	$password = $_POST['passwordsignup']; //MD5 this
+	$passwordConfirm = $_POST['passwordsignup_confirm'];//MD5 this
+	$email = $_POST['emailsignup'];
+	$gender = $_POST['gender'];
+	
+	mysqli_query($con,"INSERT INTO users (FirstName, LastName, UserName, Password, Email, Gender)
+	VALUES ('$firstName', '$lastName' , '$userName' , '$password', '$email' , '$gender')");
+	
+	echo "Record added";
 
 
-echo "First Name: " . $firstName . "\n";
-echo "Last Name: " . $lastName . "\n";
-echo "Username: " . $userName . "\n";
-echo "Password: " . $password . "\n";
-echo "Confirmed Password: " . $passwordConfirm . "\n";
-echo "Email: " . $email . "\n";
-echo "Year: " . $yearOfBirth . "\n";
-echo "Day: " . $dayOfBirth . "\n";
-echo "Month: " . $MonthOfBirth "\n";
+	mysqli_close($con);
+
+
 
 ?>

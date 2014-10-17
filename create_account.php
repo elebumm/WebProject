1fd014080faf6d614f5d4d202e2047b6226f0478
Serenity Melanson <serenitythefool@gmail.com>
2014-10-17 16:04:12 -0400

    Update create_account.php

--- create_account.php
+++ create_account.php
@@ -54,75 +54,10 @@
          <label for="emailsignup" class="youmail" data-icon="e" >Email:</label>
          <input id="emailsignup" name="emailsignup" required="required" type="email" placeholder="example@domain.com"/> 
     </p>
-    <p> 
-         <label>Date of Birth:</label>
-         <select name="month" onChange="changeDate(this.options[selectedIndex].value);">
-         <option value="na">Month</option>
-         <option value="1">January</option>
-         <option value="2">February</option>
-         <option value="3">March</option>
-         <option value="4">April</option>
-         <option value="5">May</option>
-         <option value="6">June</option>
-         <option value="7">July</option>
-         <option value="8">August</option>
-         <option value="9">September</option> 
-         <option value="10">October</option>
-         <option value="11">November</option>
-         <option value="12">December</option>
-         </select>
-         <select name="day" id="day">
-         <option value="na">Day</option>
-         </select>
-         <select name="year" id="year">
-         <option value="na">Year</option>
-         </select>
-         <script language="JavaScript" type="text/javascript">
-         function changeDate(i){
-         var e = document.getElementById('day');
-         while(e.length>0)
-         e.remove(e.length-1);
-         var j=-1;
-         if(i=="na")
-         k=0;
-         else if(i==2)
-         k=28;
-         else if(i==4||i==6||i==9||i==11)
-         k=30;
-         else
-         k=31;
-         while(j++<k){
-         var s=document.createElement('option');
-         var e=document.getElementById('day');
-         if(j==0){
-         s.text="Day";
-         s.value="na";
-         try{
-         e.add(s,null);}
-         catch(ex){
-         e.add(s);}}
-         else{
-         s.text=j;
-         s.value=j;
-         try{
-         e.add(s,null);}
-         catch(ex){
-         e.add(s);}}}}
-         y = 1998;
-         while (y-->1908){
-         var s = document.createElement('option');
-         var e = document.getElementById('year');
-         s.text=y;
-         s.value=y;
-         try{
-         e.add(s,null);}
-         catch(ex){
-         e.add(s);}}
-         </script> 
-    </p>
+        
     <p> 
          <label>Gender:</label>
-         <select name="Gender">
+         <select name="gender">
          <option value="male">Male</option>
          <option value="female">Female</option>
          </select>

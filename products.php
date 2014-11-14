0a5c69eaaef71a5246cbfe670417318a1dcab011
Lewis Menelaws <lmenelaws112@gmail.com>
2014-11-14 15:54:28 -0500

    Changed table page

--- products.php
+++ products.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
-
+<?php
+session_start();
+?>
 <html lang="en">
 <head>
 <!--
@@ -51,12 +53,38 @@ Date: Oct 31, 2014
    </div>
    
     <h1>Games</h1>
+   <?php
+
+   $conn = mysqli_connect("localhost","root","") or die(mysqli_error($conn));
+
+   mysqli_select_db($conn, "skalez_games") or die(mysqli_error($conn));
+
+   $sql = mysqli_query($conn, "SELECT * FROM products");
+
+   $fields_num = mysqli_num_fields($sql);
+
+   echo "<table id='productsTable' align='center'> <tr> 
+   <th scope='col' id='tableHead'>Picture</th>
+   <th scope='col' id='tableHead'>Title</th>
+<th scope='col' id='tableHead'>Platform</th>
+<th scope='col' id='tableHead'>Language</th>
+<th scope='col' id='tableHead'>Developer</th>
+<th scope='col' id='tableHead'>Publisher</th>
+<th scope='col' id='tableHead'>Rating</th>
+<th scope='col' id='tableHead'>Genre</th>
+<th scope='col' id='tableHead'>Price</th>
+ </tr>";
+
+while($res = mysqli_fetch_array($sql))
+ {
+	echo "<tr><td><img src='" . $res['picture_link'] .  "' width='80px' height='100px'></td><td>" . $res['title'] . "</td><td>" . $res['platform'] . "</td><td>" . $res['glanguage'] . "</td><td>" . $res['developer'] . "</td><td>" . $res['publisher'] . "</td><td>" . $res['rating'] . "</td><td>" . $res['genre'] . "</td><td>" . $res['price'] . "</td></tr>"; 
+}
+ echo "</table>";
+
+
+mysqli_close($conn);
+   ?>
    
-   <div id="productstmpl">
-   
-   	<!--Product table goes here-->
-   	
-   </div>
      
 </body>
 		

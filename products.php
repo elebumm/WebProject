185d0cd1664dd20defd04fed52d987dc0f8cef11
AshTears <slwashika@gmail.com>
2014-11-21 13:09:35 -0500

    Updated login page on mobile site

--- products.php
+++ products.php
@@ -25,26 +25,13 @@ Date: Oct 31, 2014
 	<!--Products css-->
 	<link href="css/products.css" rel="stylesheet">		
 		
-	<!--DataTables css-->
-	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.3/css/jquery.dataTables.css">
-		
-	<!-- Handlebars --> 
-	<script src="http://cloud.github.com/downloads/wycats/handlebars.js/handlebars-1.0.0.beta.6.js"></script>
-		
-	<!-- Handlebars Helpers  -->
-	<script src="js/helpers.js"></script>
-		
 	<!-- jQuery -->
 	<script src="js/jquery-2.1.1.min.js"></script>
 
-	<!--Products js-->
+	<!--Navbar js-->
 	<script src="js/navbar.js"></script>
-	<script src="js/products.js"></script>
 		
-	<!-- DataTables-->				
-	<script type="text/javascript" src="//cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
-	<script type="text/javascript" src="../../media/js/dataTables.editor.min.js"></script>
-	
+
 </head>
 
 <body>
@@ -64,20 +51,19 @@ Date: Oct 31, 2014
    $fields_num = mysqli_num_fields($sql);
 
    echo "<table id='productsTable' align='center'> <tr> 
-   <th scope='col' id='tableHead'>Picture</th>
-   <th scope='col' id='tableHead'>Title</th>
-<th scope='col' id='tableHead'>Platform</th>
-<th scope='col' id='tableHead'>Language</th>
-<th scope='col' id='tableHead'>Developer</th>
-<th scope='col' id='tableHead'>Publisher</th>
-<th scope='col' id='tableHead'>Rating</th>
-<th scope='col' id='tableHead'>Genre</th>
-<th scope='col' id='tableHead'>Price</th>
- </tr>";
+   <th scope='col' class='tableHead'>Title</th>
+	<th scope='col' class='tableHead'>Platform</th>
+	<th scope='col' class='tableHead'>Language</th>
+	<th scope='col' class='tableHead'>Developer</th>
+	<th scope='col' class='tableHead'>Publisher</th>
+	<th scope='col' class='tableHead'>Rating</th>
+	<th scope='col' class='tableHead'>Genre</th>
+	<th scope='col' class='tableHead'>Price</th>
+	 </tr>";
 
 while($res = mysqli_fetch_array($sql))
  {
-	echo "<tr><td><img src='" . $res['picture_link'] .  "' width='80px' height='100px'></td><td>" . $res['title'] . "</td><td>" . $res['platform'] . "</td><td>" . $res['glanguage'] . "</td><td>" . $res['developer'] . "</td><td>" . $res['publisher'] . "</td><td>" . $res['rating'] . "</td><td>" . $res['genre'] . "</td><td>" . $res['price'] . "</td></tr>"; 
+	echo "<tr><td>" . $res['title'] . "</td><td>" . $res['platform'] . "</td><td>" . $res['glanguage'] . "</td><td>" . $res['developer'] . "</td><td>" . $res['publisher'] . "</td><td>" . $res['rating'] . "</td><td>" . $res['genre'] . "</td><td>" . $res['price'] . "</td></tr>"; 
 }
  echo "</table>";
 
